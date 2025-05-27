d135584a26bafab6a4cef7cf47b7fd81fddc0550
nonSerp <fredfarwenel2010@gmail.com>
2025-05-27 11:55:29 +0000

    added vercel adapter for image disaplay

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [mdx(), sitemap(), icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel({
+    imageService: true
+  }),
 });
